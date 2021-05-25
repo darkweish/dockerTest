b0dc5b9e40840e5b8bcd8bfd462a879154599227
darkweish <ystephanus@laposte.net>
2021-05-25 23:25:53 +0200

    connexion to db working

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const {Sequelize} = require('sequelize')
 const app = express()
 const port = 3000
+
+const sequelize = new Sequelize('mysql://accrobat:accrobat@database:3306/database')
+sequelize.authenticate()
+.then(()=>console.log('connection succefull'))
+.catch(err => console.log(err))
 
 app.get('/',(req,res)=>{
 	res.send("Hello world")
